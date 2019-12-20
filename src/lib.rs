394451d8d4b725bb78c8247decd2a7467b45a316
CptBread <fredrik.astrom89@gmail.com>
2019-12-20 14:20:04 +0100

    SortedEntry is now a struct tuple.

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,28 @@
 use std::cmp::Ordering;
 use std::mem;
 
-pub struct SortedEntry<K, V> {
-	pub key: K,
-	pub val: V,
-}
+pub struct SortedEntry<K, V>(pub K, pub V);
 
 fn get_key<K: Ord+Clone, V>(entry: &SortedEntry<K, V>) -> K {
-	entry.key.clone()
+	entry.0.clone()
 }
 
 // Maybe remove these impl for ordering as they aren't used...
 impl <K, V> Ord for SortedEntry<K, V> where K: Ord {
 	fn cmp(&self, other: &Self) -> Ordering {
-		self.key.cmp(&other.key)
+		self.0.cmp(&other.0)
 	}
 }
 
 impl <K, V> PartialOrd for SortedEntry<K, V> where K: Ord {
 	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		Some(self.key.cmp(&other.key))
+		Some(self.0.cmp(&other.0))
 	}
 }
 
 impl <K, V> PartialEq for SortedEntry<K, V> where K: Ord {
     fn eq(&self, other: &Self) -> bool {
-		self.key == other.key
+		self.0 == other.0
 	}
 }
 
@@ -46,9 +43,9 @@ impl <K: Ord + Clone, V: Clone> SortedCollection<K, V> for Vec<SortedEntry<K, V>
 
 	fn sorted_insert(&mut self, key: K, val: V) -> Option<V> {
 		match self.sorted_searh(&key) {
-			Ok(idx) => Some(mem::replace(&mut self[idx].val, val.clone())),
+			Ok(idx) => Some(mem::replace(&mut self[idx].1, val.clone())),
 			Err(idx) => {
-				self.insert(idx, SortedEntry{key: key.clone(), val: val.clone()});
+				self.insert(idx, SortedEntry(key.clone(), val.clone()));
 				None
 			},
 		}
@@ -56,24 +53,24 @@ impl <K: Ord + Clone, V: Clone> SortedCollection<K, V> for Vec<SortedEntry<K, V>
 
 	fn sorted_get_or_add(&mut self, key: K, val: V) -> &V {
 		match self.sorted_searh(&key) {
-			Ok(idx) => &self[idx].val,
+			Ok(idx) => &self[idx].1,
 			Err(idx) => {
-				self.insert(idx, SortedEntry{key: key.clone(), val: val.clone()});
-				&self[idx].val
+				self.insert(idx, SortedEntry(key.clone(), val.clone()));
+				&self[idx].1
 			},
 		}
 	}
 
 	fn sorted_get(&self, key: K) -> Option<&V> {
 		match self.sorted_searh(&key) {
-			Ok(idx) => Some(&self[idx].val),
+			Ok(idx) => Some(&self[idx].1),
 			_ => None,
 		}
 	}
 
 	fn sorted_remove(&mut self, key: K) -> Option<V> {
 		match self.sorted_searh(&key) {
-			Ok(idx) => Some(self.remove(idx).val),
+			Ok(idx) => Some(self.remove(idx).1),
 			_ => None,
 		}
 	}
